0096cea8b186cfb8d005c7b4e47471bb12a8c074
Krishna4311 <157598323+Krishna4311@users.noreply.github.com>
2025-07-01 15:25:29 +0530

    Create libs.py

---
+++ libs.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+from datetime import datetime
+import time
+import streamlit as st
+import pymongo
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from streamlit_autorefresh import st_autorefresh
